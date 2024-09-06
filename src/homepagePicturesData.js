514a0a374e2ccfb8df85b5cfc6c8ae66d74d1a73
aidmccor <aidmccor@iu.edu>
2024-09-05 21:53:28 -0400

    fix image path in data

--- src/homepagePicturesData.js
+++ src/homepagePicturesData.js
@@ -1,7 +1,7 @@
 const homePageData = [
     {
         type: 'Mens',
-        images: ["/public/assets/images/brooke-cagle-KsBkeh3y4XM-unsplash.jpg", "/public/assets/images/mark-adriane-bO3S03I2Aw8-unsplash.jpg"],
+        images: ["/assets/images/brooke-cagle-KsBkeh3y4XM-unsplash.jpg", "/public/assets/images/mark-adriane-bO3S03I2Aw8-unsplash.jpg"],
         header: 'Comfort and Style',
         description: 'Casual pieces made for you to look and feel your best everywhere you go.'
 
@@ -9,18 +9,18 @@ const homePageData = [
     {
 
         type: 'Womens',
-        images: ['/public/assets/images/yasamine-june-GVh5yGQwiA8-unsplash.jpg', '/public/assets/images/tai-s-captures-fauoKF4zMHU-unsplash.jpg'],
+        images: ['/assets/images/yasamine-june-GVh5yGQwiA8-unsplash.jpg', '/public/assets/images/tai-s-captures-fauoKF4zMHU-unsplash.jpg'],
         header:'Clothes for Women',
         description: 'Comfortable clothes made for Everyone'
     },
 
     {
         type: 'Electronics',
-        images: ['/public/assets/images/lum3n--RBuQ2PK_L8-unsplash.jpg', '/public/assets/images/olivier-collet-hLXTRAMuvBY-unsplash.jpg']
+        images: ['/assets/images/lum3n--RBuQ2PK_L8-unsplash.jpg', '/public/assets/images/olivier-collet-hLXTRAMuvBY-unsplash.jpg']
     },
     {
         type: 'Jewlery',
-        images: ['/public/assets/images/ruan-richard-rodrigues-QwRwbZe6dWc-unsplash.jpg', '/public/assets/images/ian-talmacs-FMk8NacWplQ-unsplash.jpg']
+        images: ['/assets/images/ruan-richard-rodrigues-QwRwbZe6dWc-unsplash.jpg', '/public/assets/images/ian-talmacs-FMk8NacWplQ-unsplash.jpg']
     }
 
 ]
